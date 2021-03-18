68ba54a175c21ffc0e9c457034bc0cc429709483
teohoho <teo.sulea@gmail.com>
2021-03-18 15:20:32 +0200

    Updated test file to work with class NMAFlex

--- iMod/iModScript/test.py
+++ iMod/iModScript/test.py
@@ -1,6 +1,9 @@
-from NMAFlex import NMAFlex
+from NMAFlex import NMAtoFlex
 import mdtraj as md
 
 a = md.load("TrimerNoGlyc.inpcrd",top="TrimerNoGlyc.prmtop")
 
-NMAFlex(a, 25, "Trimer", "../iMOD_v1.04_Linux/bin/imode_gcc", deleteTemp=False, modes=[1])
+#NMAFlex(a, 25, "Trimer", "../iMOD_v1.04_Linux/bin/imode_gcc", deleteTemp=False, modes=[1])
+test = NMAtoFlex(MDTrajObj=a, iModExec="../iMOD_v1.04_Linux/bin/imode_gcc", tempRoot="./ClassTest")
+
+test.GetFlex(Output="ClassTest",Threshold=25, modes=[1,20])
